49d41fcaedc8c9829af461949b73e558b760faad
e.grobovikov <e.grobovikov@argustelecom.ru>
2023-10-24 20:07:10 +0300

    init

---
+++ src/main/java/ru/otus/project/HelloWorldApplication.java
@@ -0,0 +1,10 @@
+package ru.otus.project;
+
+/**
+ * @author zhechtec
+ */
+public class HelloWorldApplication {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
