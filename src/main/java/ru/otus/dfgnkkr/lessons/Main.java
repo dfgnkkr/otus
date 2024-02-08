84ff5ad2158f8478ab5c5cd4e5abc316b15679a8
dfgnkkr <zhechtec@ya.ru>
2024-02-08 11:39:50 +0300

    урок 28: Java IO. Часть 2
    Реализуйте метод, который подсчитывает сколько раз в текстовом файле (с кодировкой UTF-8) встречается указанная последовательность символов с учетом регистра;
    При запуске приложения пользователь вводит имя файла и искомую последовательность символов, программа должна выполнить расчет и напечатать результат в консоль.

--- src/main/java/ru/otus/dfgnkkr/lessons/Main.java
+++ src/main/java/ru/otus/dfgnkkr/lessons/Main.java
@@ -1,7 +1,35 @@
 package ru.otus.dfgnkkr.lessons;
 
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите название файла (целиком, например \"1.txt \"): ");
+        String filePath = scanner.nextLine();
+
+        System.out.print("Введите строку для поиска (например \"tEst \"): ");
+        String findStr = scanner.nextLine();
+
+        try (BufferedReader in
+                     = new BufferedReader(new FileReader(filePath))) {
+            List<String> fileLines = in.lines().collect(Collectors.toList());
+            StringBuilder builder = new StringBuilder();
+            for (String str : fileLines) {
+                builder.append(str);
+            }
+            String fullText = builder.toString();
+            System.out.println("Найдено количество вхождений: " + StringUtils.countMatches(fullText, findStr));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
