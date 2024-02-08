dc7592399e2d3a5c25091dcbfd9973cda8e90ef3
dfgnkkr <zhechtec@ya.ru>
2024-02-08 16:57:12 +0300

    урок 29: Основы многопоточности. Часть 2
    Создайте пул потоков
    Задайте пулу три задачи: первая должна 5 раз напечатать букву A, вторая - B, третья - C
    Синхронизируйте потоки в пуле таким образом, чтобы в любой ситуации в консоль печаталась последовательность ABCABCABCABCABC
    После печати всех символов программа должна завершиться

--- src/main/java/ru/otus/dfgnkkr/lessons/Main.java
+++ src/main/java/ru/otus/dfgnkkr/lessons/Main.java
@@ -1,7 +1,74 @@
 package ru.otus.dfgnkkr.lessons;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    final Object monitor = new Object();
+    String currentChar = "C";
+
+    public static void main(String[] args) throws InterruptedException {
+        Main main = new Main();
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(main::printA);
+            executorService.execute(main::printB);
+            executorService.execute(main::printC);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(30, TimeUnit.SECONDS);
+        System.out.println(".");
+        System.out.println("THE END.");
+    }
+
+    public void printA() {
+        synchronized (monitor) {
+            try {
+                while (!(currentChar.equals("C"))) {
+                    monitor.wait();
+                }
+                currentChar = "A";
+                System.out.print(currentChar);
+                monitor.notifyAll();
+                Thread.sleep(2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void printB() {
+        synchronized (monitor) {
+            try {
+                while (!(currentChar.equals("A"))) {
+                    monitor.wait();
+                }
+                currentChar = "B";
+                System.out.print(currentChar);
+                monitor.notifyAll();
+                Thread.sleep(2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (monitor) {
+            try {
+                while (!(currentChar.equals("B"))) {
+                    monitor.wait();
+                }
+                currentChar = "C";
+                System.out.print(currentChar);
+//                System.out.println("."); // так красивее
+                monitor.notifyAll();
+                Thread.sleep(2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
