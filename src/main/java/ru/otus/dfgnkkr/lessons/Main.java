e54a9a823a41e95c1b7146b7e79f09b429052e93
dfgnkkr <zhechtec@ya.ru>
2024-02-10 15:31:08 +0300

    randomizer
    вспомогательные методы... для отладки могут быть нужны...

--- src/main/java/ru/otus/dfgnkkr/lessons/Main.java
+++ src/main/java/ru/otus/dfgnkkr/lessons/Main.java
@@ -4,4 +4,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    
+
+    public static int randomInt() {
+        return randomInt(1);
+    }
+
+    public static int randomInt(int mod) {
+        return (int) (Math.random() * 10 * mod);
+    }
+
+    public static boolean randomBool() {
+        return randomInt() >= 4; // кек
+    }
 }
