4c05c72a7a41402458bae629d8dbafe8f335ab99
dfgnkkr <zhechtec@ya.ru>
2024-02-11 13:47:56 +0300

    урок 7: Основы языка Java. Часть 3

--- src/main/java/ru/otus/dfgnkkr/lessons/Main.java
+++ src/main/java/ru/otus/dfgnkkr/lessons/Main.java
@@ -1,6 +1,5 @@
 package ru.otus.dfgnkkr.lessons;
 
-import java.sql.PreparedStatement;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,17 +9,114 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        printString("спать", 4);
+        sumArrayElements(randomIntegerArrayArrays());
 
-        sumArrayElements(convertIntArray(randomIntArray()));
+        printSquad(5);
 
-        rewriteArray(convertIntArray(randomIntArray()), 5);
+        printArray(randomIntArrayArrays());
 
-        addNumToArrayElements(convertIntArray(randomIntArray()), 6);
+        findMax(randomIntArrayArrays());
 
-        sumHalfArrayElements(randomIntArray());
+        sumArrayLineElements(randomIntegerArrayArrays());
 
     }
+
+    /**
+     * Реализовать метод sumOfPositiveElements(..), принимающий в качестве аргумента целочисленный двумерный массив,
+     * метод должен посчитать и вернуть сумму всех элементов массива, которые больше 0;
+     */
+    public static void sumArrayElements(Integer[][] array) {
+        int result = 0;
+
+        for (Integer[] line : array)
+            for (Integer element : line)
+                if (element > 0) result += element;
+
+        System.out.println("_________ Сумма: " + result);
+    }
+
+    /**
+     * Реализовать метод, который принимает в качестве аргумента int size и печатает в консоль квадрат из символов * со сторонами соответствующей длины;
+     */
+    public static void printSquad(int size) {
+        // строки
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // столбцы
+                System.out.print("* ");
+            }
+            // перевод строки
+            System.out.print("\n");
+        }
+        System.out.println("_________ конец ___________");
+    }
+
+    /**
+     * Реализовать метод, принимающий в качестве аргумента двумерный целочисленный массив,
+     * и зануляющий его диагональные элементы (можете выбрать любую из диагоналей, или занулить обе);
+     */
+    public static void printArray(int[][] array) {
+        // строки
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                // столбцы
+                if (i == j) array[i][j] = 0; // зануляем диагональ
+                System.out.print(array[i][j] + " ");
+            }
+            // перевод строки
+            System.out.print("\n");
+        }
+        System.out.println("_________ конец ___________");
+    }
+
+    /**
+     * Реализовать метод findMax(int[][] array) который должен найти и вернуть максимальный элемент массива;
+     */
+    public static int findMax(int[][] array) {
+        int max = 0;
+
+        // строки
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                // столбцы
+                if (array[i][j] > max) max = array[i][j];
+                System.out.print(array[i][j] + " ");
+
+            }
+            // перевод строки
+            System.out.print("\n");
+        }
+
+        System.out.println("_________ конец ___________");
+        return max;
+    }
+
+
+    /**
+     * Реализуйте метод, который считает сумму элементов второй строки двумерного массива,
+     * если второй строки не существует, то в качестве результата необходимо вернуть -1
+     */
+    public static Integer sumArrayLineElements(Integer[][] array) {
+        Integer result = 0;
+
+        if (array.length < 2) { // если второй "строки" нет, то вернем -1
+            return -1;
+        }
+
+        List<Integer> line2 = Arrays.stream(array[1]).collect(Collectors.toList());
+
+        for (Integer num : line2) {
+            result += num;
+        }
+
+        System.out.println("Полученный массив (список): " + line2);
+        System.out.println("_________ Сумма: " + result);
+
+
+        System.out.println("_________ конец ___________");
+        return result;
+    }
+
 
     /**
      * Реализуйте метод, принимающий в качестве аргументов целое число и строку, и печатающий в
@@ -28,7 +124,7 @@ public class Main {
      */
     public static void printString(String str, int count) {
         for (int i = 0; i < count; i++) {
-            System.out.println(i+1 + ". " + str);
+            System.out.println(i + 1 + ". " + str);
         }
     }
 
@@ -39,7 +135,7 @@ public class Main {
     public static void sumArrayElements(Integer[] array) {
         Integer result = 0;
 
-        List<Integer> list =  Arrays.stream(array).filter(e -> e > 5).collect(Collectors.toList());
+        List<Integer> list = Arrays.stream(array).filter(e -> e > 5).collect(Collectors.toList());
 
         for (Integer num : list) {
             result += num;
@@ -56,7 +152,7 @@ public class Main {
      */
     public static void rewriteArray(Integer[] array, int num) {
 //        Arrays.fill(array, num); // или так
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = num;
         }
     }
@@ -66,7 +162,7 @@ public class Main {
      * массив, увеличивающий каждый элемент массива на указанное число;
      */
     public static void addNumToArrayElements(Integer[] array, int num) {
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] += num;
         }
     }
@@ -80,11 +176,11 @@ public class Main {
         int result2 = 0;
 
         int length = array.length;
-        int halfLength = length/2; // грубо округляем
+        int halfLength = length / 2; // грубо округляем
         System.out.println("___ длина массива: " + length + "___ половина длины массива: " + halfLength);
 
         int[] half1 = Arrays.copyOf(array, halfLength);
-        int[] half2 = Arrays.copyOfRange(array,halfLength - 1,length - 1);
+        int[] half2 = Arrays.copyOfRange(array, halfLength - 1, length - 1);
         System.out.println("_________ первая половина массива: " + Arrays.toString(half1));
         System.out.println("_________ вторая половина массива: " + Arrays.toString(half2));
 
@@ -108,7 +204,7 @@ public class Main {
     }
 
     public static int[] randomIntArray() {
-        return randomIntArray(11);
+        return randomIntArray(10);
     }
 
     public static int[] randomIntArray(int size) {
@@ -119,8 +215,24 @@ public class Main {
         return result;
     }
 
-    public static Integer[] convertIntArray(int[] data){
+    public static Integer[] convertIntArray(int[] data) {
         return Arrays.stream(data).boxed().toArray(Integer[]::new);
+    }
+
+    public static Integer[][] randomIntegerArrayArrays() {
+        Integer[][] array = new Integer[10][10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = convertIntArray(randomIntArray());
+        }
+        return array;
+    }
+
+    public static int[][] randomIntArrayArrays() {
+        int[][] array = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = randomIntArray();
+        }
+        return array;
     }
 
 }
